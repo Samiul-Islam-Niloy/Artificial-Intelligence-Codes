0de89473f11534966d5ac644e3022b28ffbec848
Samiul-Islam-Niloy <samiul3030@gmail.com>
2021-09-09 01:34:18 +0600

    initial commit

---
+++ Assignment 05 (Greedy Best First Search and A Star Search)/A Star Search.py
@@ -0,0 +1,126 @@
+class Graph:
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
+
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
+
+    # heuristic function with equal values for all nodes
+    def h(self, n):
+        H = {
+            'S': 10,
+            'A': 9,
+            'B': 7,
+            'C': 8,
+            'D': 8,
+            'H': 6,
+            'L': 6,
+            'F': 6,
+            'G': 3,
+            'I': 4,
+            'J': 4,
+            'K': 3,
+            'E': 0
+        }
+
+        return H[n]
+
+    def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+        li = []
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v
+                    #print(n)
+
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                li.append(n)
+                print("Visited: ", li)
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+
+                reconst_path.reverse()
+
+                print('Final Path: {}'.format(reconst_path))
+                return reconst_path
+
+            # for all neighbors of the current node do
+            for (m, weight) in self.get_neighbors(n):
+                # if the current node isn't in both open_list and closed_list
+                # add it to open_list and note n as it's parent
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update parent data and g data
+                # and if the node was in the closed_list, move it to open_list
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            li.append(n)
+            print("Visited: ", li)
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        return None
+
+
+adjacency_list = {
+    'S': [('A', 7), ('B', 2), ('C', 3)],
+    'A': [('B', 3), ('D', 4)],
+    'B': [('D', 4), ('H', 1)],
+    'C': [('L', 2)],
+    'D': [('F', 5)],
+    'H': [('F', 3), ('G', 2)],
+    'L': [('I', 4), ('J', 4)],
+    'F': [],
+    'G': [('E', 2)],
+    'I': [('K', 4)],
+    'J': [('K', 4)],
+    'K': [('E', 5)],
+    'E': []
+}
+graph1 = Graph(adjacency_list)
+graph1.a_star_algorithm('S', 'E')
